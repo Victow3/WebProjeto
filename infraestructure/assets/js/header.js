0043535086f333d66d3bbaf924eccc9e45b555fe
Victor <Victow3@gmail.com>
2025-05-12 11:25:02 -0300

    Testando o Github Pages

--- infraestructure/assets/js/header.js
+++ infraestructure/assets/js/header.js
@@ -3,12 +3,12 @@ class Header extends HTMLElement {
         this.innerHTML = `
         
         <nav>
-            <a class="logo" href="../pages/index.html">Backloggd</a>
+            <a class="logo" href="pages/index.html">Backloggd</a>
                 <ul class="nav-list">
-                    <li><a href="../pages/index.html">Inicío</a></li>
-                    <li><a href="../pages/about.html">Sobre</a></li>
-                    <li><a href="../pages/contact.html">Contato</a></li>
-                    <li><a href="../pages/atividades.html">Atividades</a></li>
+                    <li><a href="pages/index.html">Inicío</a></li>
+                    <li><a href="pages/about.html">Sobre</a></li>
+                    <li><a href="pages/contact.html">Contato</a></li>
+                    <li><a href="pages/atividades.html">Atividades</a></li>
                 </ul>
         </nav>`
     }
