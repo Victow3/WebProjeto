f0b9e2ebec3a6d64e2a5ff092bad43ee57d7a167
Victor <Victow3@gmail.com>
2025-05-12 10:36:56 -0300

    Trocando divs para tags semânticas adequadas

--- infraestructure/assets/js/header.js
+++ infraestructure/assets/js/header.js
@@ -4,12 +4,6 @@ class Header extends HTMLElement {
         
         <nav>
             <a class="logo" href="../pages/index.html">Backloggd</a>
-            <div class="mobile-menu">
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-            </div>
                 <ul class="nav-list">
                     <li><a href="../pages/index.html">Inicío</a></li>
                     <li><a href="../pages/about.html">Sobre</a></li>
