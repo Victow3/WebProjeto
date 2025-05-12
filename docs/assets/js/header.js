40b3a548b35a1fe2ad309b33175ac1732155a8ad
Victor <Victow3@gmail.com>
2025-05-12 12:32:52 -0300

    Corrigindo erros de caminho

--- docs/assets/js/header.js
+++ docs/assets/js/header.js
@@ -1,16 +1,20 @@
 class Header extends HTMLElement {
     connectedCallback() {
+        // Detecta o caminho base dinamicamente
+        const path = window.location.pathname;
+        const isInPages = path.includes("/pages/");
+        const base = isInPages ? "../" : "";
+
         this.innerHTML = `
-        
         <nav>
-            <a class="logo" href="pages/index.html">Backloggd</a>
-                <ul class="nav-list">
-                    <li><a href="pages/index.html">Inicío</a></li>
-                    <li><a href="pages/about.html">Sobre</a></li>
-                    <li><a href="pages/contact.html">Contato</a></li>
-                    <li><a href="pages/atividades.html">Atividades</a></li>
-                </ul>
-        </nav>`
+            <a class="logo" href="${base}index.html">Backloggd</a>
+            <ul class="nav-list">
+                <li><a href="${base}index.html">Início</a></li>
+                <li><a href="${base}pages/about.html">Sobre</a></li>
+                <li><a href="${base}pages/contact.html">Contato</a></li>
+                <li><a href="${base}pages/atividades.html">Atividades</a></li>
+            </ul>
+        </nav>`;
     }
 }
 
