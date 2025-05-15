6a6f3656955f0dbc515c44c4d74c0f9d791cce87
Victor <Victow3@gmail.com>
2025-05-15 14:58:01 -0300

    Corrigindo caminhos para o github pages

--- docs/assets/js/header.js
+++ docs/assets/js/header.js
@@ -1,73 +1,71 @@
 class Header extends HTMLElement {
-    connectedCallback() {
-        const path = window.location.pathname;
-        const isInPages = path.includes("/pages/");
-        const base = isInPages ? "../" : "";
+  connectedCallback() {
+    this.innerHTML = `
+      <header>
+        <nav>
+          <a class="logo" href="index.html">Backloggd</a>
 
-        this.innerHTML = `
-        <header>
-          <nav>
-            <a class="logo" href="${base}index.html">Backloggd</a>
+          <div class="mobile-menu">
+            <div class="line1"></div>
+            <div class="line2"></div>
+            <div class="line3"></div>
+          </div>
 
-            <div class="mobile-menu">
-              <div class="line1"></div>
-              <div class="line2"></div>
-              <div class="line3"></div>
-            </div>
+          <ul class="nav-list">
+            <li><a href="index.html">Início</a></li>
+            <li><a href="about.html">Sobre</a></li>
+            <li><a href="contact.html">Contato</a></li>
+            <li><a href="atividades.html">Atividades</a></li>
+          </ul>
+        </nav>
+      </header>
+    `;
 
-            <ul class="nav-list">
-              <li><a href="${base}index.html">Início</a></li>
-              <li><a href="${base}pages/about.html">Sobre</a></li>
-              <li><a href="${base}pages/contact.html">Contato</a></li>
-              <li><a href="${base}pages/atividades.html">Atividades</a></li>
-            </ul>
-          </nav>
-        </header>
-        `;
+    const mobileMenu = this.querySelector(".mobile-menu");
+    const navList = this.querySelector(".nav-list");
+    const navLinks = this.querySelectorAll(".nav-list li");
 
-        const mobileNavbar = new MobileNavbar(
-            ".mobile-menu",
-            ".nav-list",
-            ".nav-list li"
-        );
-        mobileNavbar.init(); 
-    }
+    const mobileNavbar = new MobileNavbar(mobileMenu, navList, navLinks);
+    mobileNavbar.init();
+  }
 }
 
 customElements.define('main-header', Header);
 
 class MobileNavbar {
-    constructor(mobileMenuSelector, navListSelector, navLinksSelector) {
-        this.mobileMenu = document.querySelector(mobileMenuSelector);
-        this.navList = document.querySelector(navListSelector);
-        this.navLinks = document.querySelectorAll(navLinksSelector);
-        this.activeClass = "active";
+  constructor(mobileMenuElement, navListElement, navLinksNodeList) {
+    this.mobileMenu = mobileMenuElement;
+    this.navList = navListElement;
+    this.navLinks = navLinksNodeList;
+    this.activeClass = "active";
 
-        this.handleClick = this.handleClick.bind(this);
-    }
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-    handleClick() {
-        this.navList.classList.toggle(this.activeClass);
-        this.mobileMenu.classList.toggle(this.activeClass);
-        this.animateLinks();
-    }
+  handleClick() {
+    this.navList.classList.toggle(this.activeClass);
+    this.mobileMenu.classList.toggle(this.activeClass);
+    this.animateLinks();
+  }
 
-    animateLinks() {
-        this.navLinks.forEach((link, index) => {
-            link.style.animation
-                ? (link.style.animation = "")
-                : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`);
-        });
-    }
+  animateLinks() {
+    this.navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+      }
+    });
+  }
 
-    addClickEvent() {
-        this.mobileMenu.addEventListener("click", this.handleClick);
-    }
+  addClickEvent() {
+    this.mobileMenu.addEventListener("click", this.handleClick);
+  }
 
-    init() {
-        if (this.mobileMenu) {
-            this.addClickEvent();
-        }
-        return this;
+  init() {
+    if (this.mobileMenu) {
+      this.addClickEvent();
     }
+    return this;
+  }
 }
